61383077c41a0ec77ac44b6d1e02dc4c62b694d1
Forte-NaoP <bae9750@gmail.com>
2023-11-03 23:43:30 +0900

    Update routing path

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,21 @@
 <script>
   import Router from 'svelte-spa-router'
   import Home from "./routes/Home.svelte"
-  import _Home from "./routes/Home copy.svelte"
   import Inference from "./routes/Inference.svelte"
-  import Detail from "./routes/Detail.svelte"
-  import QuestionCreate from "./routes/QuestionCreate.svelte"
   import Navigation from "./components/Navigation.svelte"
   import UserCreate from "./routes/UserCreate.svelte"
   import UserLogin from "./routes/UserLogin.svelte"
-  import QuestionModify from "./routes/QuestionModify.svelte"
   import UploadModel from './routes/UploadModel.svelte'
+  import Dashboard from './routes/Dashboard.svelte'
+  import ModelHistory from './routes/ModelHistory.svelte'
 
   const routes = {
-    '/': _Home,
-    '/detail/:question_id': Detail,
-    '/question-create': QuestionCreate,
+    '/': Dashboard,
     '/user-create': UserCreate,
     '/user-login': UserLogin,
-    '/question-modify/:question_id': QuestionModify,
-    '/upload-model': UploadModel,
-    '/inference/:model_uuid': Inference
+    '/model/upload': UploadModel,
+    '/model/:model_uuid/history': ModelHistory,
+    '/dashboard': Dashboard,
   }
 </script>
 
